50467c06f023fa6d817f7b34110ecd9094089687
zeno-samma <tranvanxuyen111@gmail.com>
2023-03-17 18:40:52 +0700

    Trigger collider

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     // Start is called before the first frame update
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)//Xet va cham
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Qua bong da va cham voi gia do");        
         }
+        else if(collision.gameObject.CompareTag("DeathZone"))
+        {
+            Debug.Log("Qua bong da va cham voi deathzone");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         if (collision.gameObject.CompareTag("DeathZone"))
         {
             Debug.Log("Qua bong da va cham voi deathzone");
