3daff3a1fe764ae57b3d597f65dc360b42a2502b
zeno-samma <tranvanxuyen111@gmail.com>
2023-03-17 11:07:03 +0700

    FirstScript

---
+++ Assets/Script/FirstScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log("Bat dau khi chuong trinh chay");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("Duoc gọi moi frame");
+    }
+}
